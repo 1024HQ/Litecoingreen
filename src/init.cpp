195a51a1b8c0e21ff586425091dce97cf4fffbfa
markerz99 <54119958+markerz99@users.noreply.github.com>
2021-01-08 11:35:23 +0700

    Revert "Revert "Update checkpoint #300000""
    This reverts commit 53846a2ee831aa5b4733ac377ecf7c494ca7bf3d.

--- src/init.cpp
+++ src/init.cpp
@@ -555,8 +555,8 @@ void SetupServerArgs()
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/litecoingreen-project/litecoingreen>";
-    const std::string URL_WEBSITE = "<https://litecoingreen.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/markerz99/Litecoingreen>";
+    const std::string URL_WEBSITE = "<https://hi.in.th>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
